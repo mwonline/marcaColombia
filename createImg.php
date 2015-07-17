3abdf05811e9932e4e4ed290d091b6633f3d5206
cristianb <cristianbautista@mwonline.com.co>
2015-07-17 13:45:52 -0500

    repositorio actualizado

---
+++ createImg.php
@@ -0,0 +1,270 @@
+<?php # Script 1
+   header('Content-Type: text/html; charset=ISO-8859-1');
+   $nombre = $_POST["nombre"];
+   $nombreExplode = explode(" ", $nombre);
+   $nombre_imagen = strtolower($nombreExplode[0]);
+
+   $nombre = ucfirst (strtolower($nombreExplode[0]));
+
+   // $nombre = strtolower($nombreExplode[0]);
+
+   $fecha = date("d/m/Y");
+   $fechaExplode = explode('/', $fecha);
+
+   switch ($fechaExplode[1]) {
+    case 01: $mes = 'Enero'; break;
+    case 02: $mes = 'Febrero'; break;
+    case 03: $mes = 'Marzo'; break;
+    case 04: $mes = 'Abril'; break;
+    case 05: $mes = 'Mayo'; break;
+    case 06: $mes = 'Junio'; break;
+    case 07: $mes = 'Julio'; break;
+    case 08: $mes = 'Agosto'; break;
+    case 09: $mes = 'Septiembre'; break;
+    case 10: $mes = 'Octubre'; break;
+    case 11: $mes = 'Noviembre'; break;
+    case 12: $mes = 'Diciembre'; break;
+   }
+
+  // Establish image factors:
+
+  // $text = $nombre." está en una relación con";
+  // $text = " está en una relación con";
+  $text = $nombre." está en una relación con";
+  $font_size = 12; // Font size is in pixels.
+  $font_file = 'css/fonts/americanTypewriter/american-typewriter.ttf'; // This is the path to your font file.
+
+  $text_colombia = " COLOMBIA.";
+  $font_size_colombia = 15; // Font size is in pixels.
+  $font_file_colombia = 'css/fonts/futuraBold/futurab.ttf'; // This is the path to your font file.
+
+  $text_fecha = $fechaExplode[0].' de '.$mes.' de '.$fechaExplode[2];
+  $font_size_fecha = 10; // Font size is in pixels.
+  $font_file_fecha = 'css/fonts/arial/arial.ttf'; // This is the path to your font file.
+
+  // Retrieve bounding box:
+  $type_space = imagettfbbox($font_size, 0, $font_file, $text);
+  $type_space = imagettfbbox($font_size_colombia, 0, $font_file_colombia, $text_colombia);
+  $type_space = imagettfbbox($font_size_fecha, 0, $font_file_fecha, $text_fecha);
+
+  // Determine image width and height, 10 pixels are added for 5 pixels padding:
+  // $image_width = abs($type_space[4] - $type_space[0]) + 10;
+  $image_width = 800;
+  $image_height = 240;
+
+  // Create image:
+  $image = imagecreatetruecolor($image_width, $image_height);
+
+  // Allocate text and background colors (RGB format):
+  $text_color = imagecolorallocate($image, 0, 0, 0);
+  $text_color_colombia = imagecolorallocate($image, 6, 85, 163);
+  $text_color_fecha = imagecolorallocate($image, 129, 129, 129);
+  $bg_color = imagecolorallocate($image, 255, 255, 255);
+
+  // Fill image:
+  imagefill($image, 0, 0, $bg_color);
+
+  // Fix starting x and y coordinates for the text:
+  // $length_name = strlen($nombre);
+  $pixels_vocals = 0;
+  for($i=0;$i<strlen($nombre);$i++){ 
+      echo $nombre[$i];
+      switch ($nombre[$i]) {
+        // MYUSCULAS
+        case 'A': $pixels_vocals = $pixels_vocals + 11; break;
+        case 'B': $pixels_vocals = $pixels_vocals + 12; break;
+        case 'C': $pixels_vocals = $pixels_vocals + 8; break;
+        case 'D': $pixels_vocals = $pixels_vocals + 11; break;
+        case 'E': $pixels_vocals = $pixels_vocals + 9; break;
+        case 'F': $pixels_vocals = $pixels_vocals + 7; break;
+        case 'G': $pixels_vocals = $pixels_vocals + 10; break;
+        case 'H': $pixels_vocals = $pixels_vocals + 12; break;
+        case 'I': $pixels_vocals = $pixels_vocals + 7; break;
+        case 'J': $pixels_vocals = $pixels_vocals + 8; break;
+        case 'K': $pixels_vocals = $pixels_vocals + 12; break;
+        case 'L': $pixels_vocals = $pixels_vocals + 8; break;
+        case 'M': $pixels_vocals = $pixels_vocals + 18; break;
+        case 'N': $pixels_vocals = $pixels_vocals + 12; break;
+        case 'O': $pixels_vocals = $pixels_vocals + 10; break;
+        case 'P': $pixels_vocals = $pixels_vocals + 14; break;
+        case 'Q': $pixels_vocals = $pixels_vocals + 13; break;
+        case 'R': $pixels_vocals = $pixels_vocals + 12; break;
+        case 'S': $pixels_vocals = $pixels_vocals + 11; break;
+        case 'T': $pixels_vocals = $pixels_vocals + 10; break;
+        case 'U': $pixels_vocals = $pixels_vocals + 12; break;
+        case 'V': $pixels_vocals = $pixels_vocals + 11; break;
+        case 'W': $pixels_vocals = $pixels_vocals + 15; break;
+        case 'X': $pixels_vocals = $pixels_vocals + 12; break;
+        case 'Y': $pixels_vocals = $pixels_vocals + 11; break;
+        case 'Z': $pixels_vocals = $pixels_vocals + 10; break;
+        // minusculas
+        case 'a': $pixels_vocals = $pixels_vocals + 10; break;
+        case 'b': $pixels_vocals = $pixels_vocals + 11; break;
+        case 'c': $pixels_vocals = $pixels_vocals + 19; break;
+        case 'd': $pixels_vocals = $pixels_vocals + 10; break;
+        case 'e': $pixels_vocals = $pixels_vocals + 8; break;
+        case 'f': $pixels_vocals = $pixels_vocals + 6; break;
+        case 'g': $pixels_vocals = $pixels_vocals + 11; break;
+        case 'h': $pixels_vocals = $pixels_vocals + 11; break;
+        case 'i': $pixels_vocals = $pixels_vocals + 6; break;
+        case 'j': $pixels_vocals = $pixels_vocals + 4; break;
+        case 'k': $pixels_vocals = $pixels_vocals + 11; break;
+        case 'l': $pixels_vocals = $pixels_vocals + 5; break;
+        case 'm': $pixels_vocals = $pixels_vocals + 16; break;
+        case 'n': $pixels_vocals = $pixels_vocals + 10; break;
+        case 'o': $pixels_vocals = $pixels_vocals + 9; break;
+        case 'p': $pixels_vocals = $pixels_vocals + 13; break;
+        case 'q': $pixels_vocals = $pixels_vocals + 12; break;
+        case 'r': $pixels_vocals = $pixels_vocals + 11; break;
+        case 's': $pixels_vocals = $pixels_vocals + 8; break;
+        case 't': $pixels_vocals = $pixels_vocals + 7; break;
+        case 'u': $pixels_vocals = $pixels_vocals + 11; break;
+        case 'v': $pixels_vocals = $pixels_vocals + 10; break;
+        case 'w': $pixels_vocals = $pixels_vocals + 14; break;
+        case 'x': $pixels_vocals = $pixels_vocals + 11; break;
+        case 'y': $pixels_vocals = $pixels_vocals + 10; break;
+        case 'z': $pixels_vocals = $pixels_vocals + 9; break;
+        // ACENTOS
+        case 'á': $pixels_vocals = $pixels_vocals + 10; break;
+        case 'à': $pixels_vocals = $pixels_vocals + 10; break;
+        case 'é': $pixels_vocals = $pixels_vocals + 8; break;
+        case 'è': $pixels_vocals = $pixels_vocals + 8; break;
+        case 'í': $pixels_vocals = $pixels_vocals + 6; break;
+        case 'ì': $pixels_vocals = $pixels_vocals + 6; break;
+        case 'ó': $pixels_vocals = $pixels_vocals + 9; break;
+        case 'ò': $pixels_vocals = $pixels_vocals + 9; break;
+        case 'Ã³': $pixels_vocals = $pixels_vocals + 15; break;
+        case 'oacute;': $pixels_vocals = $pixels_vocals + 15; break;
+        case 'ú': $pixels_vocals = $pixels_vocals + 11; break;
+        case 'ù': $pixels_vocals = $pixels_vocals + 11; break;
+        case 'Á': $pixels_vocals = $pixels_vocals + 11; break;
+        case 'À': $pixels_vocals = $pixels_vocals + 11; break;
+        case 'É': $pixels_vocals = $pixels_vocals + 9; break;
+        case 'È': $pixels_vocals = $pixels_vocals + 9; break;
+        case 'Í': $pixels_vocals = $pixels_vocals + 7; break;
+        case 'Ì': $pixels_vocals = $pixels_vocals + 7; break;
+        case 'Ó': $pixels_vocals = $pixels_vocals + 10; break;
+        case 'Ò': $pixels_vocals = $pixels_vocals + 10; break;
+        case 'Ú': $pixels_vocals = $pixels_vocals + 12; break;
+        case 'Ù': $pixels_vocals = $pixels_vocals + 12; break;
+
+      }
+  } 
+
+  mb_internal_encoding('UTF-8');
+  mb_language('uni');
+
+  # detect if the string was passed in as unicode
+  $text_encoding = mb_detect_encoding($nombre, 'UTF-8, ISO-8859-1');
+  # make sure it's in unicode
+  if ($text_encoding != 'UTF-8') {
+      $disc = mb_convert_encoding($nombre, 'UTF-8', $text_encoding);
+  }
+
+
+  $nombre_imagen = sanear_string($nombre_imagen);
+
+  $pixels_plus = $pixels_vocals / 2;
+
+  $x = 237 - $pixels_plus; // Padding of 5 pixels.
+  $y = 110; // So that the text is vertically centered.|
+
+  $x_colombia = 438 + $pixels_plus; // Padding of 5 pixels.
+
+  $y_fecha = 140;
+  $x_fecha = 335;
+
+  // Add TrueType text to image:
+  imagettftext($image, $font_size, 0, $x, $y, $text_color, $font_file, $text);
+  imagettftext($image, $font_size_colombia, 0, $x_colombia, $y, $text_color_colombia, $font_file_colombia, $text_colombia);
+  imagettftext($image, $font_size_fecha, 0, $x_fecha, $y_fecha, $text_color_fecha, $font_file_fecha, $text_fecha);
+
+  // Generate and send image to browser:
+  header('Content-type: image/png');
+  imagepng($image,'images_fb/'.$nombre_imagen.'.png');
+
+  // Destroy image in memory to free-up resources:
+  // imagedestroy($image);
+
+  // Establecer los márgenes para la estampa y obtener el alto/ancho de la imagen de la estampa
+  $margen_dcho = 10;
+  $margen_inf = 10;
+  // $sx = imagesx($estampa);
+  // $sy = imagesy($estampa);
+  $im = imagecreatefrompng('images_fb/'.$nombre_imagen.'.png');
+  $estampa = imagecreatefrompng('images/heart_status.png');
+  imagecopy($im, $estampa,385, 50, 0, 0, 30, 28);
+  // Generate and send image to browser:
+  header('Content-type: image/png');
+  imagepng($im,'images_fb/'.$nombre_imagen.'.png');
+  // imagepng($im);
+
+  // Destroy image in memory to free-up resources:
+  // imagedestroy($image);
+
+
+
+
+
+
+
+  function sanear_string($string)
+  {
+
+      $string = trim($string);
+
+      $string = str_replace(
+          array('á', 'à', 'ä', 'â', 'ª', 'Á', 'À', 'Â', 'Ä'),
+          array('a', 'a', 'a', 'a', 'a', 'A', 'A', 'A', 'A'),
+          $string
+      );
+
+      $string = str_replace(
+          array('é', 'è', 'ë', 'ê', 'É', 'È', 'Ê', 'Ë'),
+          array('e', 'e', 'e', 'e', 'E', 'E', 'E', 'E'),
+          $string
+      );
+
+      $string = str_replace(
+          array('í', 'ì', 'ï', 'î', 'Í', 'Ì', 'Ï', 'Î'),
+          array('i', 'i', 'i', 'i', 'I', 'I', 'I', 'I'),
+          $string
+      );
+
+      $string = str_replace(
+          array('ó', 'ò', 'ö', 'ô', 'Ó', 'Ò', 'Ö', 'Ô'),
+          array('o', 'o', 'o', 'o', 'O', 'O', 'O', 'O'),
+          $string
+      );
+
+      $string = str_replace(
+          array('ú', 'ù', 'ü', 'û', 'Ú', 'Ù', 'Û', 'Ü'),
+          array('u', 'u', 'u', 'u', 'U', 'U', 'U', 'U'),
+          $string
+      );
+
+      $string = str_replace(
+          array('ñ', 'Ñ', 'ç', 'Ç'),
+          array('n', 'N', 'c', 'C',),
+          $string
+      );
+
+      //Esta parte se encarga de eliminar cualquier caracter extraño
+      $string = str_replace(
+          array("\\", "¨", "º", "-", "~",
+               "#", "@", "|", "!", "\"",
+               "·", "$", "%", "&", "/",
+               "(", ")", "?", "'", "¡",
+               "¿", "[", "^", "`", "]",
+               "+", "}", "{", "¨", "´",
+               ">", "< ", ";", ",", ":",
+               ".", " "),
+          '',
+          $string
+      );
+
+
+      return $string;
+  }
+
+?>
---
+++ js/main.js
@@ -0,0 +1,490 @@
+
+
+
+// window.fbAsyncInit = function() {
+//   FB.init({
+//     appId      : '866748646748336',
+//     xfbml      : true,
+//     version    : 'v2.4'
+//   });
+//   };
+
+//   (function(d, s, id){
+//    var js, fjs = d.getElementsByTagName(s)[0];
+//    if (d.getElementById(id)) {return;}
+//    js = d.createElement(s); js.id = id;
+//    js.src = "//connect.facebook.net/en_US/sdk.js";
+//    fjs.parentNode.insertBefore(js, fjs);
+// }(document, 'script', 'facebook-jssdk'));
+
+$(document).ready(function () {
+
+  // alert(screen.width);
+
+  var inputName = '';
+  var nameFull = '';
+  var path_name = '';
+
+    $(".intro").show();
+    $(".name-step, .result, .questions,.main-header").hide();
+
+    if ($('.intro').css("visibility") == "hidden")
+    {
+        alert('intro is hidden');
+    }
+    else
+    {
+       $("body").addClass('yellow');
+       $(".url-co").addClass('co-icon-blue');
+    }
+
+     /*==========  nav top items  ==========*/
+
+    //  $(".item-navtop").on ('mouseenter', function () {
+    //    $(this).children('span').css('display', 'inherit');
+    //    $(this).children('.thumb_slider').css('display', 'inherit');
+    // });
+
+    //  $(".item-navtop").no( 'mouseleave', function () {
+    //       $(this).children('span').css('display', 'none');
+    //       $(this).children('.thumb_slider').css('display', 'none');
+    // });
+
+    $(".next-btn-arrow").click(function(event){
+      event.preventDefault();
+      $(".intro").fadeOut(300);
+      $(".slidernav-top").hide();
+      $(".main-header, .name-step").delay(400).fadeIn(1000);
+      $("body").removeClass('yellow').addClass('blue');
+      $(".url-co").removeClass('co-icon-blue').addClass('co-icon-yellow');
+      $(".arrow_right").addClass('hide_arrow');
+      $('.next-name').parent().removeClass('hide_arrow');
+    })
+
+    $(document).keypress(function(e) {
+        if(e.which == 13) {
+            inputName = remplace_caracteres($("#name").val().toLowerCase());
+            nameFull = $("#name").val();
+
+            var splitName = inputName.split(' ');
+            path_name = splitName[0];
+            path_name = path_name.toLowerCase();
+            inputName = path_name.charAt(0).toUpperCase() + path_name.slice(1);
+
+            randomString();
+
+            $(".name").text(inputName);
+            if (inputName == '' || inputName == null) {
+              console.log('Ingrese su nombre !!');
+              $("#name").attr("placeholder", "Ingresa tu nombre");
+              $("#name").addClass("error");
+              return false;
+            }
+            else{
+              event.preventDefault();
+
+              $.ajax({
+                method: "POST",
+                data: { nombre: inputName },
+                url: "createImg.php",
+              });
+
+              $(".name-step").fadeOut(100);
+              $(".name-step").removeClass('on');
+              $(".questions, .slidernav-top").show();
+              $(".first-q").addClass('on');
+              $(".first-q").fadeIn(1000);
+              $(".url-co").removeClass('co-icon-yellow').addClass('co-icon-blue');
+              $("body").css('background-color','#CE2432');
+              $(".slidernav-top ol").addClass('bg-red');
+              $(".oneli").addClass('actived');
+              if ($(".item-navtop").hasClass("actived")) {
+                $(".item-navtop.actived").children('span').css('display', 'inherit');
+                $(".item-navtop.actived").children('.thumb_slider').css('display', 'inherit');
+              }
+              $('.next-name').parent().addClass('hide_arrow');
+              $('.next-01').parent().removeClass('hide_arrow');
+              window.scrollTo(0);
+              $( document ).scrollTop( 0 );
+              $( window ).scrollTop( 0 );
+            }
+        }
+    });
+
+    $(".next-name").click(function(event){
+      inputName = remplace_caracteres($("#name").val().toLowerCase());
+      nameFull = $("#name").val();
+
+      var splitName = inputName.split(' ');
+      path_name = splitName[0];
+      path_name = path_name.toLowerCase();
+      inputName = path_name.charAt(0).toUpperCase() + path_name.slice(1);
+
+      randomString();
+
+      $(".name").text(inputName);
+      if (inputName == '' || inputName == null) {
+        console.log('Ingrese su nombre !!');
+        $("#name").attr("placeholder", "Ingresa tu nombre");
+        $("#name").addClass("error");
+        return false;
+      }
+      else{
+        event.preventDefault();
+
+        $.ajax({
+          method: "POST",
+          data: { nombre: inputName },
+          url: "createImg.php",
+        });
+
+        $(".name-step").fadeOut(100);
+        $(".name-step").removeClass('on');
+        $(".questions, .slidernav-top").show();
+        $(".first-q").addClass('on');
+        $(".first-q").fadeIn(1000);
+        $(".url-co").removeClass('co-icon-yellow').addClass('co-icon-blue');
+        $("body").css('background-color','#CE2432');
+        $(".slidernav-top ol").addClass('bg-red');
+        $(".oneli").addClass('actived');
+        if ($(".item-navtop").hasClass("actived")) {
+          $(".item-navtop.actived").children('span').css('display', 'inherit');
+          $(".item-navtop.actived").children('.thumb_slider').css('display', 'inherit');
+        }
+        $('.next-name').parent().addClass('hide_arrow');
+        $('.next-01').parent().removeClass('hide_arrow');
+
+        window.scrollTo(0);
+        $( document ).scrollTop( 0 );
+        $( window ).scrollTop( 0 );
+      }
+    })
+
+    $(".next-01").click(function(event){
+
+      $("#compartir_tw").attr('href','');
+
+      event.preventDefault();
+      $(".first-q").fadeOut(100);
+      $(".first-q").removeClass('on');
+      $(".second-q").addClass('on');
+      $(".second-q").fadeIn(1000);
+      $(".url-co").removeClass('co-icon-yellow').addClass('co-icon-blue');
+      $("body").css('background-color','#F2C245');
+      $(".slidernav-top ol").removeClass('bg-red').addClass('bg-yellow');
+      $(".oneli").removeClass('actived');
+      $(".oneli .thumb_slider, .oneli span").hide();
+      $(".twoli").addClass('actived');
+      if ($(".item-navtop").hasClass("actived")) {
+        $(".item-navtop.actived").children('span').css('display', 'inherit');
+        $(".item-navtop.actived").children('.thumb_slider').css('display', 'inherit');
+      }
+      $('.next-01').parent().addClass('hide_arrow');
+      $('.next-02').parent().removeClass('hide_arrow');
+
+      window.scrollTo(0);
+      $( document ).scrollTop( 0 );
+      $( window ).scrollTop( 0 );
+    })
+
+
+    $(".next-02").click(function(event){
+      event.preventDefault();
+      $(".second-q").fadeOut(100);
+      $(".second-q").removeClass('on');
+      $(".tirth-q").addClass('on');
+      $(".tirth-q").fadeIn(1000);
+      $(".url-co").removeClass('co-icon-blue').addClass('co-icon-red');
+      $("body").css('background-color','#234789');
+
+      $(".slidernav-top ol").removeClass('bg-yellow').addClass('bg-blue');
+      $(".twoli").removeClass('actived');
+      $(".twoli .thumb_slider, .twoli span").hide();
+      $(".threeli").addClass('actived');
+
+      if ($(".item-navtop").hasClass("actived")) {
+        $(".item-navtop.actived").children('span').css('display', 'inherit');
+        $(".item-navtop.actived").children('.thumb_slider').css('display', 'inherit');
+      }
+      $('.next-02').parent().addClass('hide_arrow');
+      $('.next-03').parent().removeClass('hide_arrow');
+
+      window.scrollTo(0);
+      $( document ).scrollTop( 0 );
+      $( window ).scrollTop( 0 );
+
+    })
+
+    $(".next-03").click(function(event){
+      event.preventDefault();
+      $(".tirth-q").fadeOut(100);
+      $(".tirth-q").removeClass('on');
+      $(".fourth-q").addClass('on');
+      $(".fourth-q").fadeIn(1000);
+      $(".url-co").removeClass('co-icon-red').addClass('co-icon-yellow');
+      $("body").css('background-color','#CE2432');
+
+      $(".slidernav-top ol").removeClass('bg-blue').addClass('bg-red');
+      $(".threeli").removeClass('actived');
+      $(".threeli .thumb_slider, .threeli span").hide();
+      $(".fourli").addClass('actived');
+
+      if ($(".item-navtop").hasClass("actived")) {
+        $(".item-navtop.actived").children('span').css('display', 'inherit');
+        $(".item-navtop.actived").children('.thumb_slider').css('display', 'inherit');
+      }
+      $('.next-03').parent().addClass('hide_arrow');
+      $('.next-04').parent().removeClass('hide_arrow');
+
+      window.scrollTo(0);
+      $( document ).scrollTop( 0 );
+      $( window ).scrollTop( 0 );
+
+    })
+
+    $(".next-04").click(function(event){
+      event.preventDefault();
+      $(".fourth-q").fadeOut(100);
+      $(".fourth-q").removeClass('on');
+      $(".fifth-q").addClass('on');
+      $(".fifth-q").fadeIn(1000);
+      $(".url-co").removeClass('co-icon-yellow').addClass('co-icon-blue');
+      $("body").css('background-color','#F2C245');
+
+      $(".slidernav-top ol").removeClass('bg-red').addClass('bg-yellow');
+      $(".fourli").removeClass('actived');
+      $(".fourli .thumb_slider, .fourli span").hide();
+      $(".fiveli").addClass('actived');
+
+      if ($(".item-navtop").hasClass("actived")) {
+        $(".item-navtop.actived").children('span').css('display', 'inherit');
+        $(".item-navtop.actived").children('.thumb_slider').css('display', 'inherit');
+      }
+      $('.next-04').parent().addClass('hide_arrow');
+      $('.next-05').parent().removeClass('hide_arrow');
+      window.scrollTo(0);
+      $( document ).scrollTop( 0 );
+      $( window ).scrollTop( 0 );
+    })
+
+    $(".next-05").click(function(event){
+      event.preventDefault();
+
+      $(".fifth-q").fadeOut(100);
+      $(".fifth-q").removeClass('on');
+      $(".sixth-q").addClass('on');
+      $(".sixth-q").fadeIn(1000);
+      $(".url-co").removeClass('co-icon-blue').addClass('co-icon-red');
+      $("body").css('background-color','#234789');
+
+      $(".slidernav-top ol").removeClass('bg-yellow').addClass('bg-blue');
+      $(".fiveli").removeClass('actived');
+      $(".fiveli .thumb_slider, .fiveli span").hide();
+      $(".sixli").addClass('actived');
+
+      if ($(".item-navtop").hasClass("actived")) {
+        $(".item-navtop.actived").children('span').css('display', 'inherit');
+        $(".item-navtop.actived").children('.thumb_slider').css('display', 'inherit');
+      }
+      $('.next-05').parent().addClass('hide_arrow');
+      $('.next-result').parent().removeClass('hide_arrow');
+      window.scrollTo(0);
+      $( document ).scrollTop( 0 );
+      $( window ).scrollTop( 0 );
+    })
+
+    $(".next-result").click(function(event){
+      event.preventDefault();
+
+      var dispositivo = navigator.userAgent.toLowerCase();
+      
+      if( dispositivo.search(/iphone|ipod|ipad/) > -1 ){
+        // alert('celular no es android');
+        $('#compartir_tw').attr('href','http://twitter.com/share?text=¡Yo estoy %23enunarelacionconCOlombia! Descubre si tú también lo estás, ingresando aquí:');
+      }
+      else if( dispositivo.search(/android/) > -1 ){
+        // alert('share android');
+        $('#compartir_tw').attr('href','http://twitter.com/share?text=¡Yo estoy %23enunarelacionconCOlombia! Descubre si tú también lo estás, ingresando aquí:http://20dejulio.colombia.co');
+      }
+      else{
+        // alert('pc');
+        $('#compartir_tw').attr('href','https://twitter.com/?status=¡Yo estoy %23enunarelacionconCOlombia! Descubre si tú también lo estás, ingresando aquí:http://20dejulio.colombia.co');
+      }
+
+
+      $('.main-header').find('h2').addClass('hideTextPhone');
+
+      $(".sixth-q").fadeOut(100);
+      $(".sixth-q").removeClass('on');
+      $(".result").fadeIn(1000);
+      $(".slidernav-top").hide();
+      $("body").css('background-color','#F2C245');
+      $(".arrow_right, .arrow_right_last").hide();
+
+      var infoUser = {
+        'name'    : nameFull,
+        'qOne'    : $('input[name=q1]:checked', '#formQuestionOne').val(),
+        'qTwo'    : $('input[name=q2]:checked', '#formQuestionTwo').val(),
+        'qThree'  : $('input[name=q3]:checked', '#formQuestionThree').val(),
+        'qFour'   : $('input[name=q4]:checked', '#formQuestionFour').val(),
+        'qFive'   : $('input[name=q5]:checked', '#formQuestionFive').val(),
+        'qSix'    : $('input[name=q6]:checked', '#formQuestionSix').val()
+      }
+      ajaxInsertQuestion(infoUser);
+
+      window.scrollTo(0);
+      $( document ).scrollTop( 0 );
+      $( window ).scrollTop( 0 );
+
+      
+
+
+    })
+
+    if($(".first-q").hasClass("on")){
+      $("body").css('background-color','#CE2432');
+    }else{
+      $(".first-q").hide();
+    }
+
+    if($(".second-q").hasClass("on")){
+      $("body").css('background-color','#F2C245');
+    }else{
+      $(".second-q").hide();
+    }
+
+    if($(".tirth-q").hasClass("on")){
+      $("body").css('background-color','#234789');
+    }else{
+      $(".tirth-q").hide();
+    }
+
+    if($(".fourth-q").hasClass("on")){
+      $("body").css('background-color','#CE2432');
+    }else{
+      $(".fourth-q").hide();
+    }
+
+
+    if($(".fifth-q").hasClass("on")){
+      $("body").css('background-color','#F2C245');
+    }else{
+      $(".fifth-q").hide();
+    }
+
+    if($(".sixth-q").hasClass("on")){
+      $("body").css('background-color','#234789');
+    }else{
+      $(".sixth-q").hide();
+    }
+
+
+    $('#compartir_fb').on('click',function(){
+
+    //   FB.ui({
+    //     method: 'share',
+    //     href: '20dejulio.colombia.co/index2.html',
+    //     picture: '20dejulio.colombia.co/images_fb/'+path_name+'.png',
+    //     title: '¡DESCUBRE SI TÚ TAMBIÉN ESTÁS EN UNA RELACIÓN CON COLOMBIA!',
+    //   }, function(response){});
+    //   return false;
+
+    // });
+
+      FB.ui({
+        method: 'feed',
+        link: '20dejulio.colombia.co/',
+        picture: '20dejulio.colombia.co/images_fb/'+path_name+'.png',
+        // title: '¡DESCUBRE SI TÚ TAMBIÉN ESTÁS EN UNA RELACIÓN CON COLOMBIA!',
+        name: '¡DESCUBRE SI TÚ TAMBIÉN ESTÁS EN UNA RELACIÓN CON COLOMBIA!',
+      }, function(response){});
+
+
+    });
+});
+
+function randomString(){
+  var b = 1;
+  var a = 3;
+  var aleatorio = Math.round(Math.random()*(b-a)+parseInt(a));
+  console.log(aleatorio);
+  if (aleatorio == 1) {
+    $(".msg-final").text('¡Son el complemento perfecto!');
+    $(".msg-second").text('Sigue alimentando ese amor todos los días. ');
+  }
+  if (aleatorio == 2) {
+    $(".msg-final").text('¡Son la combinación ideal!');
+    $(".msg-second").text('Tú y Colombia hacen un gran equipo juntos');
+  }
+  if (aleatorio == 3) {
+    $(".msg-final").text('¡Son el uno para el otro!');
+    $(".msg-second").text('Tú y COLOMBIA van juntos por buenas cosas.');
+  }
+}
+
+myFunction();
+function myFunction() {
+    var month = new Array();
+    month[0] = "Enero";
+    month[1] = "Febrero";
+    month[2] = "Marzo";
+    month[3] = "Abril";
+    month[4] = "Mayo";
+    month[5] = "Junio";
+    month[6] = "Julio";
+    month[7] = "Augosto";
+    month[8] = "Septiembre";
+    month[9] = "Octubre";
+    month[10] = "Noviembre";
+    month[11] = "Diciembre";
+
+    var date = new Date();
+    var mes = month[date.getMonth()];
+    var dia = date.getDate();
+    var year = date.getFullYear();
+    var fecha = dia + ' de ' + mes + ' de ' + year;
+    $('.date-txt').text(fecha);
+}
+
+function remplace_caracteres(nombre)
+{
+  // nombre=nombre.replace('Á','A');
+  // nombre=nombre.replace('á','a');
+  // nombre=nombre.replace('à','a');
+  // nombre=nombre.replace('É','E');
+  // nombre=nombre.replace('é','e');
+  // nombre=nombre.replace('è','e');
+  // nombre=nombre.replace('Í','I');
+  // nombre=nombre.replace('í','i');
+  // nombre=nombre.replace('ì','i');
+  // nombre=nombre.replace('Ó','O');
+  // nombre=nombre.replace('ó','o');
+  // nombre=nombre.replace('ò','o');
+  // nombre=nombre.replace('Ú','U');
+  // nombre=nombre.replace('ú','u');
+  // nombre=nombre.replace('ù','u');
+  // nombre=nombre.replace('Ñ','N');
+  // nombre=nombre.replace('ñ','n');
+  nombre=nombre.replace('Ä','A');
+  nombre=nombre.replace('ä','a');
+  nombre=nombre.replace('Ë','E');
+  nombre=nombre.replace('ë','e');
+  nombre=nombre.replace('Ï','I');
+  nombre=nombre.replace('ï','i');
+  nombre=nombre.replace('Ö','O');
+  nombre=nombre.replace('ö','o');
+  nombre=nombre.replace('Ü','U'); 
+  nombre=nombre.replace('ü','u'); 
+  return nombre;
+}
+
+function ajaxInsertQuestion(infoUser){
+
+
+  $.ajax({
+    method: "POST",
+    data: { infoUser: infoUser },
+    url: "insertUser.php",
+  });
+
+}
